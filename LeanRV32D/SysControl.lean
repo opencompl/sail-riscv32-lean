721947f8b9a0c3ca7332e14222b556ff650c04f8
ineol <ineol@users.noreply.github.com>
2026-08-20 05:18:31 +0000

    Update main to output generated at

--- LeanRV32D/SysControl.lean
+++ LeanRV32D/SysControl.lean
@@ -609,7 +609,7 @@ def getPendingSet (priv : Privilege) : SailM (Option ((BitVec 32) × Privilege))
     else (pure none))
 
 def shouldWakeForInterrupt (_ : Unit) : SailM Bool := do
-  (pure (((← readReg mip) &&& (← readReg mie)) != (zeros (n := 32))))
+  (pure (((← (read_mip IncludePlatformInterrupts)) &&& (← readReg mie)) != (zeros (n := 32))))
 
 def dispatchInterrupt (priv : Privilege) : SailM (Option (InterruptType × Privilege)) := do
   match (← (getPendingSet priv)) with
